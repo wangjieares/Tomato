ec8955da262ae08edc3ee4d01a935e9165784848
wangjie <wangjie1004406930@126.com>
2017-10-18 19:34:31 +0800

    2017.10.18

--- app/src/main/java/www/atomato/com/tomato/LaunchActivity.java
+++ app/src/main/java/www/atomato/com/tomato/LaunchActivity.java
@@ -3,6 +3,7 @@ package www.atomato.com.tomato;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.LinearLayout;
 
 import www.atomato.com.tomato.activity.MainActivity;
@@ -12,18 +13,29 @@ import www.atomato.com.tomato.data.YouMiSdk;
 
 public class LaunchActivity extends Activity {
     LinearLayout mLinearLayout;
+    private Handler mHandler = new Handler();
+    private Intent mIntent;
+
+    //延时执行
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
         mLinearLayout = (LinearLayout)findViewById(R.id.activity_launch_linear) ;
-        if(AdBehaior.LaunchAd()){
-            YouMiSdk youMiSdk = new YouMiSdk(this);
-            youMiSdk.initSplash(mLinearLayout);
-        }else {
-            Intent intent = new Intent(this,MainActivity.class);
-            startActivity(intent);
-        }
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(AdBehaior.LaunchAd()){
+                    YouMiSdk youMiSdk = new YouMiSdk(LaunchActivity.this);
+                    youMiSdk.initSplash(mLinearLayout);
+                }else {
+                    Intent intent = new Intent(LaunchActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+    }, 3000);//3000微妙后跳转
+
     }
 
     @Override
