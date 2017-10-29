7cb99edbc6589fa2c226b5c26e22c7d7d0be3bf0
wangjie <wangjie1004406930@126.com>
2017-10-29 16:38:32 +0800

    Remind

--- app/src/main/java/www/atomato/com/tomato/service/RemindService.java
+++ app/src/main/java/www/atomato/com/tomato/service/RemindService.java
@@ -25,6 +25,21 @@ public class RemindService extends BaseService {
         Intent intent = new Intent(this, AddItemActivity.class);
         mPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
+//        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+//        //读者可以修改此处的Minutes从而改变提醒间隔时间
+//        //此处是设置每隔90分钟启动一次
+//        //这是90分钟的毫秒数
+//        int Minutes = 90*60*1000;
+//        //SystemClock.elapsedRealtime()表示1970年1月1日0点至今所经历的时间
+//        long triggerAtTime = SystemClock.elapsedRealtime() + Minutes;
+//        //此处设置开启AlarmReceiver这个Service
+//        Intent i = new Intent(this, AlarmReceiver.class);
+//        PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+//        //ELAPSED_REALTIME_WAKEUP表示让定时任务的出发时间从系统开机算起，并且会唤醒CPU。
+//        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+//        return super.onStartCommand(intent, flags, startId);
+
+
     }
 
     @Nullable
